de6c0984a2cc03cd0a93fe9cfb4831e99915a9a2
Shayna Daimond <shaynalernerdiamond@gmail.com>
2019-05-04 18:41:14 -0400

    set up canvas js and test first number positioning

---
+++ CIQ_assessment.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+    var canvas = document.getElementById("IBM-closing-chart");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "14px Arial";
+    ctx.fillText("24",10,20);
+
+  });
