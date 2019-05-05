d3e72a31f38071a1e687e257da76ac813f8c7ba7
Shayna Daimond <shaynalernerdiamond@gmail.com>
2019-05-05 17:46:01 -0400

    workign with calculating y

--- CIQ_assessment.js
+++ CIQ_assessment.js
@@ -138,91 +138,169 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //render moving average of closing price     
     ctx.font = "10px Arial";
-    ctx.beginPath()
+    ctx.beginPath();
 
     //line to chart the moving average
     let ave_label_position_x = 85;
-    let moving_average_point = 95;
-    ctx.fillText(calculate_moving_average(0),ave_label_position_x,335);
-    ctx.moveTo(moving_average_point, 335);
+    let x_axis_location = 95;
+
+
+    function calculate_y_coordinates(i){
+        //GRID HEIGHT = 540px
+        /*
+        Y = Chart_max( today_avg / max_avg )
+        Y = 500 px * 80% = 400 px
+        */
+       console.log(calculate_moving_average(i));
+        let ave_percent =  (calculate_moving_average(i) / 22);
+        // let y_axis_location =  (690-(420 * ave_percent)).toFixed(2);
+        // let y_axis_location =  (760-(540 * ave_percent)).toFixed(2);
+        let y_axis_location =  760-(540 * ave_percent).toFixed(2);
+        console.log('ave_percent = ' + ave_percent);
+        console.log('y_axis_location = ' + y_axis_location);
+        console.log(' ');
+        
+        return y_axis_location;
+    }
+
+    // ctx.fillText(calculate_moving_average(0),ave_label_position_x,335);
+    // ctx.moveTo(x_axis_location, 335);
    
-    ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(1),ave_label_position_x,325);
-    ctx.lineTo(moving_average_point, 325);
+    ctx.fillText(calculate_moving_average(0),ave_label_position_x,335);
+    ctx.moveTo(x_axis_location, calculate_y_coordinates(0));
+    // console.log('ave_percent = ' + ave_percent);
+    // console.log('FIRST y_axis_location = ' + calculate_y_coordinates());
+
+    // ave_label_position_x = ave_label_position_x + x_axis_increment;
+    // x_axis_location = x_axis_location + x_axis_increment;    ctx.fillText(calculate_moving_average(1),ave_label_position_x,325);
+    // ctx.lineTo(x_axis_location, 325);
 
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(2),ave_label_position_x,330);
-    ctx.lineTo(moving_average_point, 330);
+    x_axis_location = x_axis_location + x_axis_increment;    
+    
+    ctx.fillText(calculate_moving_average(1),ave_label_position_x,325);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(1));
+    // console.log('ave_percent = ' + ave_percent);
+    // console.log(' SECOND y_axis_location = ' + calculate_y_coordinates());
 
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(3),ave_label_position_x,315);
-    ctx.lineTo(moving_average_point, 315);
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(2),ave_label_position_x,330);
+    // ctx.lineTo(x_axis_location, 330);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(2));
+    // console.log(' THIRD y_axis_location = ' + calculate_y_coordinates());
+
+
 
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(4),ave_label_position_x,322);
-    ctx.lineTo(moving_average_point, 322);
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(3),ave_label_position_x,315);
+    // ctx.lineTo(x_axis_location, 315);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(3));
+
 
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(5),ave_label_position_x,310);
-    ctx.lineTo(moving_average_point, 310);
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(4),ave_label_position_x,322);
+    // ctx.lineTo(x_axis_location, 322);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(4));
+
 
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(6),ave_label_position_x,308);
-    ctx.lineTo(moving_average_point, 308);
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(5),ave_label_position_x,310);
+    // ctx.lineTo(x_axis_location, 310);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(5));
+
 
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(7),ave_label_position_x,305);
-    ctx.lineTo(moving_average_point, 305);
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(6),ave_label_position_x,308);
+    // ctx.lineTo(x_axis_location, 308);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(6));
+
+
+    ave_label_position_x = ave_label_position_x + x_axis_increment;
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(7),ave_label_position_x,305);
+    // ctx.lineTo(x_axis_location, 305);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(7));
+
     
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(8),ave_label_position_x,295);
-    ctx.lineTo(moving_average_point, 295);
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(8),ave_label_position_x,295);
+    // ctx.lineTo(x_axis_location, 295);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(8));
+
    
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(9),ave_label_position_x,285);
-    ctx.lineTo(moving_average_point, 285);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(9),ave_label_position_x,285);
+    // ctx.lineTo(x_axis_location, 285);
+   ctx.lineTo(x_axis_location, calculate_y_coordinates(9));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(10),ave_label_position_x,280);
-    ctx.lineTo(moving_average_point, 280);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(10),ave_label_position_x,280);
+    // ctx.lineTo(x_axis_location, 280);
+   ctx.lineTo(x_axis_location, calculate_y_coordinates(10));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(11),ave_label_position_x,275);
-    ctx.lineTo(moving_average_point, 275);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(11),ave_label_position_x,275);
+    // ctx.lineTo(x_axis_location, 275);
+   ctx.lineTo(x_axis_location, calculate_y_coordinates(11));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(12),ave_label_position_x,265);
-    ctx.lineTo(moving_average_point, 265);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(12),ave_label_position_x,265);
+    // ctx.lineTo(x_axis_location, 265);
+   ctx.lineTo(x_axis_location, calculate_y_coordinates(12));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(13),ave_label_position_x,260);
-    ctx.lineTo(moving_average_point, 260);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(13),ave_label_position_x,260);
+    // ctx.lineTo(x_axis_location, 260);
+   ctx.lineTo(x_axis_location, calculate_y_coordinates(13));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(14),ave_label_position_x,250);
-    ctx.lineTo(moving_average_point, 250);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(14),ave_label_position_x,250);
+    // ctx.lineTo(x_axis_location, 250);
+   ctx.lineTo(x_axis_location, calculate_y_coordinates(14));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(15),ave_label_position_x,245);
-    ctx.lineTo(moving_average_point, 245);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(15),ave_label_position_x,245);
+    // ctx.lineTo(x_axis_location, 245);
+   ctx.lineTo(x_axis_location, calculate_y_coordinates(15));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(16),ave_label_position_x,240);
-    ctx.lineTo(moving_average_point, 240);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(16),ave_label_position_x,240);
+    // ctx.lineTo(x_axis_location, 240);
+   ctx.lineTo(x_axis_location, calculate_y_coordinates(16));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(17),ave_label_position_x,230);
-    ctx.lineTo(moving_average_point, 230);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(17),ave_label_position_x,230);
+    // ctx.lineTo(x_axis_location, 230);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(17));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(18),ave_label_position_x,225);
-    ctx.lineTo(moving_average_point, 225);
-   
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(18),ave_label_position_x,225);
+    // ctx.lineTo(x_axis_location, 225);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(18));
+
     ave_label_position_x = ave_label_position_x + x_axis_increment;
-    moving_average_point = moving_average_point + x_axis_increment;    ctx.fillText(calculate_moving_average(19),ave_label_position_x,220);
-    ctx.lineTo(moving_average_point, 220);
+    x_axis_location = x_axis_location + x_axis_increment;    
+    ctx.fillText(calculate_moving_average(19),ave_label_position_x,220);
+    // ctx.lineTo(x_axis_location, 220);
+    ctx.lineTo(x_axis_location, calculate_y_coordinates(19));
 
     ctx.strokeStyle = "#f00";
     ctx.lineWidth = 1;
     ctx.stroke();
-  })  
+  })
