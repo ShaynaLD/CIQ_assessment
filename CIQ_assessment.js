5a489028893b315a082d71e506c8d04b78f28212
Shayna Daimond <shaynalernerdiamond@gmail.com>
2019-05-05 00:09:13 -0400

    refactor rest of grid with math

--- CIQ_assessment.js
+++ CIQ_assessment.js
@@ -3,70 +3,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var canvas = document.getElementById("IBM-closing-chart");
     var ctx = canvas.getContext("2d");
 
+    //Draw the Grid
     ctx.beginPath()
-    ctx.moveTo(45,10);
-    ctx.lineTo(45,550);
-    ctx.lineTo(990,550);
+        ctx.moveTo(45,10);
+        ctx.lineTo(45,550);
+        ctx.lineTo(990,550);
     ctx.stroke();
-
     ctx.beginPath()
-    //horizontal, whole numbers rules
-    ctx.moveTo(45,20);
-    ctx.lineTo(990,20);
-    ctx.moveTo(45,90);
-    ctx.lineTo(990,90);
-    ctx.moveTo(45,160);
-    ctx.lineTo(990,160);
-    ctx.moveTo(45,240);
-    ctx.lineTo(990,240);
-    ctx.moveTo(45,310);
-    ctx.lineTo(990,310);
-    ctx.moveTo(45,380);
-    ctx.lineTo(990,380);
-    ctx.moveTo(45,450);
-    ctx.lineTo(990,450);
-    ctx.moveTo(45,520);
-    ctx.lineTo(990,520);
-
-    //vertical, whole numbers rules
-    let x =95;
-    ctx.moveTo(x,10);
-    ctx.lineTo(x,550);
-    while(x <= 999){
-        x = x+45;
-        console.log(x);
+        //horizontal, whole numbers rules
+        let y =20;
+        ctx.moveTo(45,y);
+        ctx.lineTo(990,y);
+        while(y < 500 ){
+            y = y+70;
+            console.log(y);
+            ctx.moveTo(45,y);
+            ctx.lineTo(990,y);
+        }
+        //vertical, whole numbers rules
+        let x =95;
         ctx.moveTo(x,10);
         ctx.lineTo(x,550);
-    }
-
-    // ctx.lineTo(990,520);
-    ctx.strokeStyle = "#ccc";
-    ctx.lineWidth = 1;
+        while(x <= 930){
+            x = x+45;
+            console.log(x);
+            ctx.moveTo(x,10);
+            ctx.lineTo(x,550);
+        }
+        ctx.strokeStyle = "#333";
+        ctx.lineWidth = .5;
     ctx.stroke();
-    
-
     ctx.beginPath()
-    //horizontal, fractional numbers rules
-    ctx.moveTo(45,55);
-    ctx.lineTo(990,55);
-    ctx.moveTo(45,125);
-    ctx.lineTo(990,125);
-    ctx.moveTo(45,195);
-    ctx.lineTo(990,195);
-    ctx.moveTo(45,275);
-    ctx.lineTo(990,275);
-    ctx.moveTo(45,345);
-    ctx.lineTo(990,345);
-    ctx.moveTo(45,415);
-    ctx.lineTo(990,415);
-    ctx.moveTo(45,485);
-    ctx.lineTo(990,485);
-    // ctx.moveTo(45,520);
-
-    ctx.lineTo(990,520);ctx.strokeStyle = "#ddd";
-    ctx.lineWidth = 1;
+        //horizontal, fractional numbers rules
+        let yf =37.5;
+        ctx.moveTo(45,yf);
+        ctx.lineTo(990,yf);
+        while(yf < 530 ){
+            yf = yf+17.5;
+            console.log(yf);
+            ctx.moveTo(45,yf);
+            ctx.lineTo(990,yf);
+        }
+        ctx.lineTo(990,520);ctx.strokeStyle = "#ddd";
+        ctx.lineWidth = 1;
     ctx.stroke();
 
+
+    //add price scall along y-axis
     ctx.font = "14px Arial";
     ctx.fillText("$22",20,20);
     ctx.fillText("$21",20,90);
@@ -76,31 +59,52 @@ document.addEventListener("DOMContentLoaded", function(event) {
     ctx.fillText("$17",20,380);
     ctx.fillText("$16",20,450);
     ctx.fillText("$15",20,520);
+    //add dates along x-axis
+    let date_lable = 85;
+    const date_spacer = 45
+    ctx.fillText("4/5",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/8",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/9",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/10",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/11",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/12",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/15",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/16",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/17",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/18",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/22",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/23",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/24",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/25",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/26",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/29",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("4/30",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("5/1",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("5/2",date_lable,570);
+        date_lable = date_lable + date_spacer;
+    ctx.fillText("5/3",date_lable,570);
 
-    ctx.fillText("4/5",50,570);
-    ctx.fillText("4/8",95,570);
-    ctx.fillText("4/9",140,570);
-    ctx.fillText("4/10",185,570);
-    ctx.fillText("4/11",230,570);
-    ctx.fillText("4/12",275,570);
-    ctx.fillText("4/15",320,570);
-    ctx.fillText("4/16",365,570);
-    ctx.fillText("4/17",410,570);
-    ctx.fillText("4/18",455,570);
-    ctx.fillText("4/22",500,570);
-    ctx.fillText("4/23",545,570);
-    ctx.fillText("4/24",590,570);
-    ctx.fillText("4/25",635,570);
-    ctx.fillText("4/26",680,570);
-    ctx.fillText("4/29",725,570);
-    ctx.fillText("4/30",765,570);
-    ctx.fillText("5/1",810,570);
-    ctx.fillText("5/2",855,570);
-    ctx.fillText("5/3",900,570);
 
-
+    //calculate moving average of closing price
     var closing_price_array=[ 17.52, 17.86, 17.55, 18.24, 17.54, 18.63, 18.09, 18.46, 19.23, 19.50, 19.17, 19.51, 19.88, 19.91, 20.47, 20.39, 20.24, 20.83, 20.75, 20.95, ];
-    var moving_average_array = []
     function calculate_moving_average(i){
         // console.log('');
         // console.log('i = ' + i );
@@ -127,6 +131,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // console.log('calculate_moving_average(2) = ' + calculate_moving_average(2));
     // console.log(' for i=2, sum=52.93, ave=17.64');
 
+    //render moving average of closing price     
+    ctx.font = "10px Arial";
     ctx.fillText(calculate_moving_average(0),50,200);
     ctx.fillText(calculate_moving_average(1),95,200);
     ctx.fillText(calculate_moving_average(2),140,200);
