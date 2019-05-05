4139c2d43d467ddfb50401123e4ff2e4d9776f32
Shayna Daimond <shaynalernerdiamond@gmail.com>
2019-05-05 10:41:07 -0400

    remove consol logs and unnecessary comments

--- CIQ_assessment.js
+++ CIQ_assessment.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         ctx.lineTo(990,y);
         while(y < 500 ){
             y = y+70;
-            console.log(y);
             ctx.moveTo(45,y);
             ctx.lineTo(990,y);
         }
@@ -26,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         ctx.lineTo(x,550);
         while(x <= 930){
             x = x+45;
-            console.log(x);
             ctx.moveTo(x,10);
             ctx.lineTo(x,550);
         }
@@ -40,14 +38,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
         ctx.lineTo(990,yf);
         while(yf < 530 ){
             yf = yf+17.5;
-            console.log(yf);
             ctx.moveTo(45,yf);
             ctx.lineTo(990,yf);
         }
         ctx.lineTo(990,520);ctx.strokeStyle = "#ddd";
         ctx.lineWidth = 1;
     ctx.stroke();
-
 
     //add price scall along y-axis
     ctx.font = "14px Arial";
@@ -59,6 +55,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     ctx.fillText("$17",20,380);
     ctx.fillText("$16",20,450);
     ctx.fillText("$15",20,520);
+
     //add dates along x-axis
     let date_lable = 85;
     const date_spacer = 45
@@ -102,96 +99,111 @@ document.addEventListener("DOMContentLoaded", function(event) {
         date_lable = date_lable + date_spacer;
     ctx.fillText("5/3",date_lable,570);
 
-
     //calculate moving average of closing price
     var closing_price_array=[ 17.52, 17.86, 17.55, 18.24, 17.54, 18.63, 18.09, 18.46, 19.23, 19.50, 19.17, 19.51, 19.88, 19.91, 20.47, 20.39, 20.24, 20.83, 20.75, 20.95, ];
     function calculate_moving_average(i){
-        // console.log('');
-        // console.log('i = ' + i );
         var moving_average;
         if(i===0){
             moving_average = (closing_price_array[0]);
         }
         else if(i>0){
             let prices_to_average=closing_price_array.slice(0,i+1);
-            // console.log('prices_to_average = ' + prices_to_average);
             let sum_of_prices=prices_to_average.reduce(add,0)
             function add(accumulator, a) {
                 return accumulator + a;
             }
-            // console.log('sum_of_prices = ' + sum_of_prices);
             moving_average = ((sum_of_prices)/(i + 1)).toFixed(2);
-            // console.log('moving_average in if = '  + moving_average);
         }
-        // console.log('closing_price_array[' + i + '] = ' + closing_price_array[i] );
         return moving_average;
     }
-    // console.log('calculate_moving_average(0) = ' + calculate_moving_average(0));
-    // console.log('calculate_moving_average(1) = ' + calculate_moving_average(1));
-    // console.log('calculate_moving_average(2) = ' + calculate_moving_average(2));
-    // console.log(' for i=2, sum=52.93, ave=17.64');
 
     //render moving average of closing price     
     ctx.font = "10px Arial";
+    ctx.beginPath()
 
+    //line to chart the moving average
     let ave_lable_position_x = 85;
-
+    let moving_average_point = 95;
     ctx.fillText(calculate_moving_average(0),ave_lable_position_x,335);
+    ctx.moveTo(moving_average_point, 335);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(1),ave_lable_position_x,325);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(1),ave_lable_position_x,325);
+    ctx.lineTo(moving_average_point, 325);
+
+    ave_lable_position_x = ave_lable_position_x + date_spacer;
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(2),ave_lable_position_x,330);
+    ctx.lineTo(moving_average_point, 330);
+
+    ave_lable_position_x = ave_lable_position_x + date_spacer;
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(3),ave_lable_position_x,315);
+    ctx.lineTo(moving_average_point, 315);
+
+    ave_lable_position_x = ave_lable_position_x + date_spacer;
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(4),ave_lable_position_x,322);
+    ctx.lineTo(moving_average_point, 322);
+
+    ave_lable_position_x = ave_lable_position_x + date_spacer;
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(5),ave_lable_position_x,310);
+    ctx.lineTo(moving_average_point, 310);
+
+    ave_lable_position_x = ave_lable_position_x + date_spacer;
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(6),ave_lable_position_x,308);
+    ctx.lineTo(moving_average_point, 308);
+
+    ave_lable_position_x = ave_lable_position_x + date_spacer;
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(7),ave_lable_position_x,305);
+    ctx.lineTo(moving_average_point, 305);
+    
+    ave_lable_position_x = ave_lable_position_x + date_spacer;
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(8),ave_lable_position_x,295);
+    ctx.lineTo(moving_average_point, 295);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(2),ave_lable_position_x,330);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(9),ave_lable_position_x,285);
+    ctx.lineTo(moving_average_point, 285);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(3),ave_lable_position_x,315);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(10),ave_lable_position_x,280);
+    ctx.lineTo(moving_average_point, 280);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(4),ave_lable_position_x,322);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(11),ave_lable_position_x,275);
+    ctx.lineTo(moving_average_point, 275);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(5),ave_lable_position_x,310);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(12),ave_lable_position_x,265);
+    ctx.lineTo(moving_average_point, 265);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(6),ave_lable_position_x,308);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(13),ave_lable_position_x,260);
+    ctx.lineTo(moving_average_point, 260);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(7),ave_lable_position_x,305);
-      
-    ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(8),ave_lable_position_x,295);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(14),ave_lable_position_x,250);
+    ctx.lineTo(moving_average_point, 250);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(9),ave_lable_position_x,285);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(15),ave_lable_position_x,245);
+    ctx.lineTo(moving_average_point, 245);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(10),ave_lable_position_x,280);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(16),ave_lable_position_x,240);
+    ctx.lineTo(moving_average_point, 240);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(11),ave_lable_position_x,275);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(17),ave_lable_position_x,230);
+    ctx.lineTo(moving_average_point, 230);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(12),ave_lable_position_x,265);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(18),ave_lable_position_x,225);
+    ctx.lineTo(moving_average_point, 225);
    
     ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(13),ave_lable_position_x,260);
-   
-    ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(14),ave_lable_position_x,250);
-   
-    ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(15),ave_lable_position_x,245);
-   
-    ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(16),ave_lable_position_x,240);
-   
-    ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(17),ave_lable_position_x,230);
-   
-    ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(18),ave_lable_position_x,225);
-   
-    ave_lable_position_x = ave_lable_position_x + date_spacer;
-    ctx.fillText(calculate_moving_average(19),ave_lable_position_x,220);
+    moving_average_point = moving_average_point + date_spacer;    ctx.fillText(calculate_moving_average(19),ave_lable_position_x,220);
+    ctx.lineTo(moving_average_point, 220);
+
+    ctx.strokeStyle = "#f00";
+    ctx.lineWidth = 1;
+    ctx.stroke();
   })  
